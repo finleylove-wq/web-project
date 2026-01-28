60191b03c880faf214b578703dae0b2392b4115d
Spare 2 <Finley Love>
2026-01-28 14:35:47 -0500

    message

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int count[10];
+    count10[i] = i;
+for(int i=0; i<=10; i++);
+printf("%d\n",count10[i];
+}
+int main(){
+    int count[20];
+    count20[i] = i;
+for(int i=2, i<=20; i++);
+printf("%d\n"count20[i]);
+}
